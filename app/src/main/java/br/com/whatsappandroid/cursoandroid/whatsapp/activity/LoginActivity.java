132f8d6d2ee142fa6e7f688279bb5ebcb6053c49
Joberth Rogers <joberth.rogers18@gmail.com>
2018-07-23 00:56:20 -0300

    take informations from EditText
    Replace characteres from mask for manipulate
    
    Generate a token radom

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/LoginActivity.java
@@ -2,10 +2,15 @@ package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import java.util.Random;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
 
@@ -15,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText codigoPais;
     private EditText codigoArea;
     private EditText nome;
+    private Button cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
         telefone = findViewById(R.id.editTelefone);
         codigoPais = findViewById(R.id.editCodPais);
         codigoArea = findViewById(R.id.editCodArea);
+        cadastrar = findViewById(R.id.btCadastrar);
 
         // Definir mascaras
 
@@ -39,5 +46,32 @@ public class LoginActivity extends AppCompatActivity {
         SimpleMaskFormatter simpleCodArea = new SimpleMaskFormatter("NN");
         MaskTextWatcher maskCodArea = new MaskTextWatcher(codigoArea, simpleCodArea);
         codigoArea.addTextChangedListener(maskCodArea);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nomeUsuario = nome.getText().toString();
+                String telefoneCompleto = codigoPais.getText().toString() +
+                        codigoArea.getText().toString() +
+                        telefone.getText().toString();
+
+                String telefoneSemFormatacao = telefoneCompleto.replace("+","");
+                telefoneSemFormatacao = telefoneSemFormatacao.replace("-","");
+
+                Log.i("Telefone:", telefoneSemFormatacao);
+
+                //Gerar Token
+
+                Random randomico = new Random();
+                int numeroRandomico = randomico.nextInt(9999 - 1000) + 1000; // tokens apenas com quatro digitos
+
+                String token = String.valueOf(numeroRandomico);
+
+                Log.i("TOKEN", token);
+
+
+            }
+        });
     }
 }
