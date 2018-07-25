2d0eb1cc4b7d70065c3305aceb22a5dd92fcfd96
Joberth Rogers <joberth.rogers18@gmail.com>
2018-07-25 19:14:41 -0300

    Adding button to Sign out and change to Login activity

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
@@ -1,15 +1,39 @@
 package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoSair;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoSair = findViewById(R.id.bt_sair);
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                autenticacao = ConfiguracaoFirebase.getFireBaseAuth();
+                autenticacao.signOut(); //desloga o user
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent); //muda a activty
+
+            }
+        });
     }
 }
