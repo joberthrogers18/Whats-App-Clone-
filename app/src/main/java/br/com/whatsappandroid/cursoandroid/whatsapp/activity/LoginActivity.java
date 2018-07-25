df918c25936cbf32cfb632648023c1198b79f7e5
Joberth Rogers <joberth.rogers18@gmail.com>
2018-07-25 19:03:00 -0300

    Verify the data about user and Sign in him

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/LoginActivity.java
@@ -2,21 +2,79 @@ package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
+import br.com.whatsappandroid.cursoandroid.whatsapp.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText email;
+    private EditText senha;
+    private Button botaoLogar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        email = findViewById(R.id.edit_login_email);
+        senha = findViewById(R.id.edit_login_senha);
+        botaoLogar = findViewById(R.id.bt_logar);
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                usuario = new Usuario();
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+                validarLogin();
+
+            }
+        });
+
+    }
+
+    private void validarLogin(){
+
+        autenticacao  = ConfiguracaoFirebase.getFireBaseAuth();
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            abriTelaPricipal();
+                            Toast.makeText(LoginActivity.this, "Sucesso ao fazer login!", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Erro ao fazer login!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+
+    private void abriTelaPricipal(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void abrirCadastroUsuario(View view){
