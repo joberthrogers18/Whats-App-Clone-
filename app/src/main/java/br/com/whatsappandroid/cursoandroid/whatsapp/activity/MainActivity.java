78e0be6b4fad3665a06f0d6aaa79683a696ba0dc
Joberth Rogers <joberth.rogers18@gmail.com>
2018-07-25 22:58:17 -0300

    Adding method OnCreateOptionsMenu for load menu created
    activity main include the toobar

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,13 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
     private Button botaoSair;
     private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = findViewById(R.id.bt_sair);
+        toolbar = findViewById(R.id.toobar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar); //metodo de suporte da actionbar, para funcionar corretamente
+
+       /* botaoSair = findViewById(R.id.bt_sair);
 
         botaoSair.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +42,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent); //muda a activty
 
             }
-        });
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //metodo chamado para carregar o menu
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu); // parametros: menu_main e o menu padrão do metodo
+
+        return true;
     }
 }
